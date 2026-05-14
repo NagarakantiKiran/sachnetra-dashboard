d871daf69e7a0ac70577c493703faa62dea69333
Kiranhub-max <nagarakantikiran@gmail.com>
2026-05-14 22:31:17 +0530

    disabled caching - DONE

--- app/api/stats/route.js
+++ app/api/stats/route.js
@@ -1,6 +1,9 @@
 import { query } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET() {
   try {
     const [todayStats, weekStats, sentimentStats, marketMoving, threatHigh] =
